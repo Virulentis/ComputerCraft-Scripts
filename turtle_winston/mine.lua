47f29afa9008aa5c5970173dcc8edc979fed3540
Wramsay <61526309+Virulentis@users.noreply.github.com>
2025-11-06 23:11:21 -0800

    Updated shouldReturnToBase to check to see if it can make it back to fuel source.

--- turtle_winston/mine.lua
+++ turtle_winston/mine.lua
@@ -1,9 +1,10 @@
 require("nav")
 require("state_manager")
-
+require("utils")
 
 function mainLoop(row, col)
     local starting_face = getCurrentDirection()
+    local 
     row = tonumber(row)
     col = tonumber(col)
     print("col: "..col.." row: "..row)
@@ -14,6 +15,8 @@ function mainLoop(row, col)
                 turtle.dig()
                 turtle.forward() 
             end
+            
+            shouldReturnToBase("operations")
             if i < row then
                 if isLastTurnRight then
                     turnRight()
@@ -43,17 +46,18 @@ function shouldReturnToBase(waypoint)
 
     local pos = {}
     pos.x, pos.y, pos.z = gps.locate()
-    print("position z")
-    -- print(pos.z)
-    
     local w_pos = loadState(waypoint)
-    print("Original Position -> x: "..pos.x.." y: "..pos.y.." z: "..pos.z)
-    print("Waypoint Position -> x: "..w_pos.x.." y: "..w_pos.y.." z: "..w_pos.z)
     
     local fuelMovementCost = math.abs(pos.x - w_pos.x) + math.abs(pos.y - w_pos.y) + math.abs(pos.z - w_pos.z)
-    print(fuelMovementCost)
+    if fuelMovementCost + 200 > turtle.getFuelLevel() then
+        saveState("temp_pos")
+        moveWaypoint(waypoint)
+        dropAllItemsDown()
+        suckUpAndRefuel()
+        moveWaypoint("temp_pos")
+    end
+    
 
 end
 
-shouldReturnToBase("operations")
 mainLoop(arg[1], arg[2])
